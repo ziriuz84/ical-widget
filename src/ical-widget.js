1ce0f45c377d28479dce9bedc3b3249cbc8222b2
Sirio Negri <ziriuz84@gmail.com>
2022-06-18 11:34:28 +0200

    Main constant for the app

---
+++ src/ical-widget.js
@@ -0,0 +1,4 @@
+const ICAL_URL =
+  "https://calendar.google.com/calendar/ical/7c69dp628skp9gf6mlbtsp70nc%40group.calendar.google.com/public/basic.ics";
+const DAY_NUMBER = 10;
+const EVENT_NUMBER = 10;
